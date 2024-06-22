83d3fbd6f3cc573e06d10778eb94423c7e6dedde
Armaan Mishra <armaanmishra48@gmail.com>
2024-06-23 02:04:16 +0530

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
+import { ExternalLink } from "lucide-react";
 import Image from "next/image";
-
 export default function Home() {
-  return <div>Grainify</div>;
+  return (
+    <MaxWidthWrapper>
+      <div className="min-h-screen flex flex-col items-center justify-center p-4 text-center gap-8">
+        <div className="max-w-7xl flex flex-col items-center justify-center gap-y-8">
+          <h1 className="text-5xl md:text-7xl font-bold max-w-3xl">
+            Add Vintage Charm to Your Photos
+          </h1>
+          <Button className="mt-5 w-30  font-bold text-md" size={"lg"}>
+            Try it out
+            <ExternalLink className="w-5 h-5 ml-2" />
+          </Button>
+        </div>
+        <div>
+          <Image
+            priority={true}
+            src={"/homephoto.jpg"}
+            alt="demo photo"
+            width={700}
+            height={700}
+          />
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import ThemeToggle from "./ThemeToggle";
+
+const NavBar = () => {
+  return (
+    <nav className="sticky top-0 z-50 border-b border-base-200 bg-base-100 p-1 backdrop-blur-lg">
+      <MaxWidthWrapper>
+        <div className="flex items-center justify-between flex-wrap p-2">
+          <Link href={"/"} className="font-semibold text-2xl pl-2">
+            Grainify
+          </Link>
+          <ThemeToggle />
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default NavBar;
