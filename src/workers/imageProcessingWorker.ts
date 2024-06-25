ad39a26950e899e68dd81af0cdfddeaba83fa83e
Armaan Mishra <armaanmishra48@gmail.com>
2024-06-25 16:48:23 +0530

    created worker for image processing

---
+++ src/workers/imageProcessingWorker.ts
@@ -0,0 +1,19 @@
+self.onmessage = (e: MessageEvent) => {
+  const { imageData, amount } = e.data;
+
+  const data = new Uint8ClampedArray(imageData.data);
+  const length = data.length;
+  const halfAmount = amount / 2;
+
+  for (let i = 0; i < length; i += 4) {
+    const grain = Math.random() * amount - halfAmount;
+    data[i] = Math.min(255, Math.max(0, data[i] + grain)); // Red
+    data[i + 1] = Math.min(255, Math.max(0, data[i + 1] + grain)); // Green
+    data[i + 2] = Math.min(255, Math.max(0, data[i + 2] + grain)); // Blue
+    // Alpha channel remains unchanged
+  }
+
+  self.postMessage({
+    processedData: new ImageData(data, imageData.width, imageData.height),
+  });
+};
