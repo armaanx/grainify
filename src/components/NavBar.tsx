953136761f10cdd661e637110b640caf0941187c
Armaan Mishra <armaanmishra48@gmail.com>
2024-07-07 02:48:34 +0530

    updated NavBar style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 
 const NavBar = () => {
   return (
-    <nav className="sticky bg-background blurBg  z-50 top-0 left-0 right-0 border-b shadow-sm flex items-center justify-between ">
+    <nav className="sticky bg-background   z-50 top-0 left-0 right-0 border-b shadow-sm flex items-center justify-between ">
       <MaxWidthWrapper>
         <div className="flex items-center justify-between flex-wrap p-2">
           <Link href={"/"} className="font-semibold text-2xl pl-2">
