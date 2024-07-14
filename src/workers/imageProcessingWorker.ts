ebe5a18b84d0ced6c03674d0ec8699dc10dadb9c
Armaan Mishra <armaanmishra48@gmail.com>
2024-07-14 23:01:54 +0530

    tweaked worker to support filters

--- src/workers/imageProcessingWorker.ts
+++ src/workers/imageProcessingWorker.ts
@@ -1,30 +1,16 @@
+import filters from "@/lib/filterFunctions";
+
 self.onmessage = (e) => {
-  const { imageData, amount, grainType, grayscale } = e.data;
-  const data = new Uint8ClampedArray(imageData.data);
+  const { imageData, amount, grainType, filterNum } = e.data;
+
+  const filteredData =
+    filterNum !== undefined
+      ? filters[filterNum].function()(imageData)
+      : imageData;
+
+  const data = new Uint8ClampedArray(filteredData.data);
   const length = data.length;
   const halfAmount = amount / 2;
-
-  if (grayscale) {
-    for (let i = 0; i < length; i += 4) {
-      const red = data[i];
-      const green = data[i + 1];
-      const blue = data[i + 2];
-
-      // Using the luminance formula to convert to grayscale
-      const grayscale = 0.299 * red + 0.587 * green + 0.114 * blue;
-
-      // Set the RGB channels to the grayscale value
-      data[i] = grayscale; // Red
-      data[i + 1] = grayscale; // Green
-      data[i + 2] = grayscale; // Blue
-    }
-    if (amount == 0) {
-      self.postMessage({
-        processedData: new ImageData(data, imageData.width, imageData.height),
-      });
-      return;
-    }
-  }
 
   if (grainType === "monochrome" && amount > 0) {
     for (let i = 0; i < length; i += 4) {
